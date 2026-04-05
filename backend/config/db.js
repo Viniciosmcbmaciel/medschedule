b8beb15c3f48b8ce804f15748bb4e404c03c0af2
Viniciosmcbmaciel <vinicios.mcastelobranco@gmail.com>
2026-04-05 14:26:02 -0300

    Corrigindo conexão com MySQL usando pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error("Erro ao conectar no banco:", err);
-    } else {
-        console.log("Conectado ao MySQL");
-    }
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
