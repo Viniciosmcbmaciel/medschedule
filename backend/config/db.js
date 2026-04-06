ad059432bb87fafefae021b65515b30f821f8ae3
Viniciosmcbmaciel <vinicios.mcastelobranco@gmail.com>
2026-04-06 20:34:23 -0300

    Usando MYSQL direto no backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -16,6 +16,11 @@ db.getConnection((err, connection) => {
         console.error("ERRO AO CONECTAR NO MYSQL:", err);
     } else {
         console.log("MYSQL CONECTADO COM SUCESSO");
+        console.log({
+            host: process.env.MYSQLHOST,
+            user: process.env.MYSQLUSER,
+            database: process.env.MYSQLDATABASE
+        });
         connection.release();
     }
 });
