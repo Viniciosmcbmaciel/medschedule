e4a14238d87b84645349955ed24feaeab34ec5df
Viniciosmcbmaciel <vinicios.mcastelobranco@gmail.com>
2026-04-07 19:36:02 -0300

    Usando DATABASE_URL do Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,18 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+    console.error("DATABASE_URL não definida");
+}
+
+const db = mysql.createPool(databaseUrl);
 
 db.getConnection((err, connection) => {
     if (err) {
         console.error("ERRO AO CONECTAR NO MYSQL:", err);
     } else {
         console.log("MYSQL CONECTADO COM SUCESSO");
-        console.log({
-            host: process.env.MYSQLHOST,
-            user: process.env.MYSQLUSER,
-            database: process.env.MYSQLDATABASE
-        });
         connection.release();
     }
 });
